7701133e2377b72733c6a857c623fcb2dd1a940d
Nikhil <nikstvp@gmail.com>
2015-04-14 16:43:18 -0700

    changed dispatcher servlet

---
+++ src/main/java/com/sjsu/cmpe275/lab3/daoimpl/SponsorDaoImpl.java
@@ -0,0 +1,31 @@
+package com.sjsu.cmpe275.lab3.daoimpl;
+
+import java.sql.SQLException;
+
+import com.sjsu.cmpe275.lab3.dao.SponsorDao;
+import com.sjsu.cmpe275.lab3.model.Sponsor;
+
+public class SponsorDaoImpl implements SponsorDao {
+
+	public void createSponsor(Sponsor sponsor) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Sponsor getSponsor(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateSponsor(int sponsorId, Sponsor sponsor)
+			throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteSponsor(int id) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
